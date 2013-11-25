674381a6f96801f4134d85c8f5abcdb7a9d59c84
Friker <viktor.tomkovic@gmail.com>
2013-11-25 13:48:08 +0100

    chapter 7 algorithm 35 implemented ... and added
    Signed-off-by: Friker <viktor.tomkovic@gmail.com>

---
+++ src/algorithm/chapter7/Algorithm35.java
@@ -0,0 +1,97 @@
+package algorithm.chapter7;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+import model.Graph;
+import algorithm.AbstractMDSAlgorithm;
+
+public class Algorithm35 implements AbstractMDSAlgorithm {
+	private LinkedHashMap<Long, Algorithm35State> allVertices = new LinkedHashMap<>();
+	private LinkedList<Algorithm35State> unfinishedVertices = new LinkedList<>();
+	private LinkedHashSet<Long> S = new LinkedHashSet<>();
+	public Object joinLock = new Object();
+
+	public Algorithm35() {
+	}
+
+	@Override
+	public LinkedHashSet<Long> mdsAlg(Graph g) {
+		Long start = System.currentTimeMillis();
+		LinkedList<Long> times = new LinkedList<>();
+		int bla = (int) Math.ceil(g.getNumberOfVertices() * 1.5);
+		allVertices = new LinkedHashMap<>(bla);
+		Long nv = g.getNumberOfVertices();
+		for (Long v : g.getVertices()) {
+			Algorithm35State state = new Algorithm35State(v, g);
+			unfinishedVertices.add(state);
+			allVertices.put(v, state);
+		}
+		times.addLast(System.currentTimeMillis() - start);
+		Long nt = Math.min(nv, 1000);
+		System.out.println(nv);
+
+		// variant 1
+		ArrayList<Thread> pool = new ArrayList<>();
+		for (int i = 0; i < nt; i++) {
+			Thread t = new Thread(new Algorithm35Task(this));
+			pool.add(t);
+		}
+		times.addLast(System.currentTimeMillis() - start);
+		for (Thread t : pool) {
+			t.start();
+		}
+		times.addLast(System.currentTimeMillis() - start);
+		for (Thread t : pool) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		times.addLast(System.currentTimeMillis() - start);
+
+		/*
+		 * // variant 2 Thread[] pool = new Thread[nv.intValue()]; for (int i =
+		 * 0; i < nt; i++) { Thread t = new Thread(new Algorithm34Task(this));
+		 * pool[i] = t; } times.addLast(System.currentTimeMillis() - start); for
+		 * (int i = 0; i < nt; i++) { pool[i].start(); }
+		 * times.addLast(System.currentTimeMillis() - start);
+		 * 
+		 * for (int i = 0; i < nt; i++) { try { pool[i].join(); } catch
+		 * (InterruptedException e) { e.printStackTrace(); } }
+		 * times.addLast(System.currentTimeMillis() - start);
+		 */
+		System.out.println("Time elapsed: " + times);
+		return S;
+	}
+
+	public LinkedHashMap<Long, Algorithm35State> getAllVertices() {
+		return this.allVertices;
+	}
+
+	public Algorithm35State chooseNextVertex() {
+		Algorithm35State state;
+		synchronized (unfinishedVertices) {
+			state = unfinishedVertices.pollFirst();
+		}
+		return state;
+	}
+
+	public void saveState(Algorithm35State state) {
+		synchronized (unfinishedVertices) {
+			unfinishedVertices.addLast(state);
+		}
+		return;
+	}
+
+	public void joinS(Long v) {
+		synchronized (S) {
+			S.add(v);
+		}
+		return;
+	}
+
+}
---
+++ src/algorithm/chapter7/Algorithm35Task.java
@@ -0,0 +1,207 @@
+package algorithm.chapter7;
+
+import java.util.LinkedHashMap;
+
+public class Algorithm35Task implements Runnable {
+	private Algorithm35 alg;
+	private Algorithm35State state;
+	private LinkedHashMap<Long, Algorithm35State> vertices;
+	private boolean finished;
+
+	public Algorithm35Task(Algorithm35 alg) {
+		this.alg = alg;
+		this.vertices = alg.getAllVertices();
+		this.finished = false;
+	}
+
+	private void saveState() {
+		alg.saveState(state);
+		setState(null);
+		return;
+	}
+
+	private void setNextState() {
+		setState(alg.chooseNextVertex());
+		if (state == null) {
+			finished = true;
+		}
+		return;
+	}
+
+	private boolean hasWhiteNeighbours(Long v) {
+		boolean result;
+		synchronized (state.W) {
+			state.W.remove(v);
+			result = !state.W.isEmpty();
+			state.W.add(v);
+		}
+		return result;
+	}
+
+	public Long computeSpan() {
+		int w = 0;
+		synchronized (state.W) {
+			w = state.W.size();
+		}
+		Double a = Math.pow(2, Math.floor(Math.log(w) / Math.log(2)));
+		long result = a.intValue();
+		return result;
+	}
+
+	public boolean recievedFromAll() {
+		boolean b = false;
+		synchronized (state.dist2NotSorG) {
+			synchronized (state.spans) {
+				b = state.spans.keySet().containsAll(state.dist2NotSorG);
+			}
+		}
+		return b;
+	}
+
+	private void joinS() {
+		synchronized (alg.joinLock) {
+			LinkedHashMap<Long, Algorithm35State> states = alg.getAllVertices();
+			synchronized (state.dist2NotSorG) {
+				for (Long v : state.dist2NotSorG) {
+					states.get(v).takeABreakePlease();
+				}
+				for (Long v : state.dist2NotSorG) {
+					Algorithm35State s = states.get(v);
+					s.recieveRemoveFromW(state.v);
+					if (state.v != v) {
+						s.recieveRemoveFromDist2(state.v);
+					}
+				}
+				alg.joinS(state.v);
+				for (Long v : state.dist2NotSorG) {
+					states.get(v).thankYou();
+				}
+			}
+		}
+		return;
+	}
+
+	private void joinG() {
+		synchronized (alg.joinLock) {
+			LinkedHashMap<Long, Algorithm35State> states = alg.getAllVertices();
+			synchronized (state.dist2NotSorG) {
+				for (Long v : state.dist2NotSorG) {
+					states.get(v).takeABreakePlease();
+				}
+				for (Long v : state.dist2NotSorG) {
+					Algorithm35State s = alg.getAllVertices().get(v);
+					s.recieveRemoveFromW(state.v);
+					if (state.v != v) {
+						s.recieveRemoveFromDist2(state.v);
+					}
+				}
+				for (Long v : state.dist2NotSorG) {
+					states.get(v).thankYou();
+				}
+			}
+		}
+		return;
+	}
+
+	@Override
+	public void run() {
+		setNextState();
+		while (!finished) {
+			if (hasWhiteNeighbours(state.v)) {
+				state.w = computeSpan();
+				synchronized (state.dist2NotSorG) {
+					for (Long v2 : state.dist2NotSorG) {
+						vertices.get(v2).recieveSpan(state.v, state.w);
+					}
+				}
+				if (recievedFromAll()) {
+					state.w = computeSpan();
+
+					long maxOtherSpan = maxFromOthers();
+					if (maxOtherSpan <= state.w) {
+						synchronized (state.isCandidate) {
+							state.isCandidate = true;
+						}
+					}
+					state.c = computeC();
+					if (joinTest()) {
+						joinS();
+						setNextState();
+						if (finished)
+							return;
+					}
+					state.spans.clear();
+				}
+				try {
+					Thread.sleep(0, 10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} else {
+				joinG();
+				// System.out.println("koniec- " + state.v);
+				setNextState();
+				if (finished)
+					return;
+			}
+			saveState();
+			setNextState();
+			if (finished)
+				return;
+		}
+		return;
+	}
+
+	private boolean joinTest() {
+		boolean b = state.isCandidate;
+		long sum = 0L;
+		synchronized (state.W) {
+			for (Long v : state.W) {
+				Algorithm35State s = vertices.get(v);
+				synchronized (s.c) {
+					sum = sum + s.c;
+				}
+			}
+		}
+		boolean c;
+		synchronized (state.w) {
+			c = sum <= 3 * state.w;
+		}
+		return b && c;
+	}
+
+	private Long computeC() {
+		long c = 0L;
+		synchronized (state.N) {
+			for (Long v : state.N) {
+				Algorithm35State s = vertices.get(v);
+				synchronized (s.isCandidate) {
+					if (s.isCandidate)
+						c++;
+				}
+			}
+		}
+		return c;
+	}
+
+	private long maxFromOthers() {
+		long max = 0L;
+		synchronized (state.spans) {
+			for (Long l : state.spans.values()) {
+				if (l > max) {
+					max = l;
+				}
+			}
+		}
+		return max;
+	}
+
+	public Algorithm35State getState() {
+		return state;
+	}
+
+	public void setState(Algorithm35State state) {
+		this.state = state;
+	}
+
+}
